a17e8712c8cf0028d4892588a48f8b77da6a4bc9
Khan, Awais <awais.khan@accenture.com>
2020-06-28 22:13:00 +0200

    workin on site

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Renderer2, Inject } from '@angular/core';
+import { DOCUMENT } from '@angular/common';
 
 @Component({
   selector: 'app-home',
@@ -7,8 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(@Inject(DOCUMENT) document, public r: Renderer2) {
+    r.addClass(document.body, 'white-background');
+  }
 
   ngOnInit(): void {
+  }
+
+  ngOnDestroy() {
+    this.r.removeClass(document.body, 'white-background');
   }
 }
